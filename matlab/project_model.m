1b06794cf6ead539a87660d9bec6627f59e94f57
jimmy <jimmy@westlake.cs.ubc.ca>
2017-07-12 08:44:16 -0700

    update project_model as a function

--- matlab/project_model.m
+++ matlab/project_model.m
@@ -1,3 +1,54 @@
+function project_model(camera, model_points, model_line_segment, rgb_image)
+% project a 2D field model to the image space
+% camera: 9 camera parameters, (u, v, focal_length, Rx, Rx, Rz, Cx, Cy, Cz)
+%         (Rx, Rx, Rz): is Rodrigues angle, (Cx, Cy, Cz) unit in meter
+% model_points: N x 2 matrix, model world coordinate in meter
+% model_line_segment: line segment index, start from 1
+% rgb_image: an RGB image
+% example: 
+% camera = [640.000000	 360.000000	 2986.943295	 1.367497	 -1.082443	 0.980122	 -16.431519	 14.086604	 5.580546];
+% I = imread('00003600.jpg');
+% load('soccer_field_model.mat');
+% model_points = points;
+% model_line_segment = line_segment_index + 1;
+% project_model(camera, model_points, model_line_segment, I);
+assert(length(camera) == 9);
+assert(size(model_points, 2) == 2);
+assert(size(model_line_segment, 2) == 2);
+assert(size(rgb_image, 3) == 3);
+
+% 
+u = camera(1);
+v = camera(2);
+f = camera(3);
+rod = camera(4:6); % Rodrigues angle
+c = camera(7:9)';
+
+K = [f, 0, u; 0, f, v; 0, 0, 1];
+rotation = rotationVectorToMatrix(rod)'; % camera rotation to world point rotation
+
+N = size(model_points, 1);
+image_points = zeros(N, 2);
+
+for i = [1:N]
+    p = [model_points(i, 1), model_points(i, 2), 0.0]';
+    % translate, rotate, then project
+    p = K*rotation*(p - c);
+    x = p(1)/p(3);
+    y = p(2)/p(3);  % p(3) may be zero
+    image_points(i, :) = [x, y];    
+end
+
+% draw projected model on the image
+figure; imshow(rgb_image);
+hold on;
+idx1 = model_line_segment(:, 1); % linesegment start index
+idx2 = model_line_segment(:, 2); % linesegment end index
+x = [image_points(idx1, 1), image_points(idx2, 1)];
+y = [image_points(idx1, 2), image_points(idx2, 2)];
+plot(x', y', 'r');
+
+end
 %{
 %% project model line
 clear
