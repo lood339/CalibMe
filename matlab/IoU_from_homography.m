8dfb0498b3203d8f4e426e5d7abb4e9685596c7b
jimmy <jimmy@westlake.cs.ubc.ca>
2017-07-14 08:19:08 -0700

    add IoU_from_homography.m (empty) file

---
+++ matlab/IoU_from_homography.m
@@ -0,0 +1,8 @@
+%IoU from a homography
+function iou = IoU_from_homography(h, sz1, sz2)
+% h: 3 x 3 homography matrix, from image 1 to image2
+% sz1: [width, height] of image 1
+% sz2: [width, height] of image 2
+% iou: intersection over union of the warp
+
+end
