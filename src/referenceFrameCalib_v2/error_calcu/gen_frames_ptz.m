602a7290827bb88ac24ea1b97657644e8e7a140e
zfrruirui <xiaoruiruisen@126.com>
2017-08-10 10:34:45 -0700

    modify files

--- src/referenceFrameCalib_v2/error_calcu/gen_frames_ptz.m
+++ src/referenceFrameCalib_v2/error_calcu/gen_frames_ptz.m
@@ -4,7 +4,7 @@ load camera_frames_sort_seq2.mat
 seq2_frames_ptz = {};
 for i = 1:size(camera_frames_sort,2)
     cc = camera_frames_sort(i).para(end-2:end);
-    base_rotation = camera_frames_sort(i).para(4:6);
+    base_rotation = [1.278234, 1.118293, -1.103959];
     camera = camera_frames_sort(i).para(3:end);
     seq2_frames_ptz(end+1).img_name = camera_frames_sort(i).img_name;
     seq2_frames_ptz(end).camera = camera;
--- src/referenceFrameCalib_v2/error_calcu/gen_ground_truth_ptz.m
+++ src/referenceFrameCalib_v2/error_calcu/gen_ground_truth_ptz.m
@@ -6,7 +6,7 @@ ground_truth_ptz = {};
 for i = 950:5:995
     [image_name, camera] = readCamera(strcat('/home/ritazhu/sports/CalibMe-master/matlab/soccer_1_seq2_annotation/0',num2str(i),'_manual_calib.txt'));
     cc = camera(end-2:end);
-    base_rotation = camera(4:6);
+    base_rotation = [1.278234, 1.118293, -1.103959];
     camera1 = camera(3:end);
     ground_truth_ptz(end+1).img_name = image_name;
     ground_truth_ptz(end).para = camera2PTZ(cc, base_rotation, camera1);
@@ -15,7 +15,7 @@ end
 for i = 1000:5:1065
     [image_name, camera] = readCamera(strcat('/home/ritazhu/sports/CalibMe-master/matlab/soccer_1_seq2_annotation/',num2str(i),'_manual_calib.txt'));
     cc = camera(end-2:end);
-    base_rotation = camera(4:6);
+    base_rotation = [1.278234, 1.118293, -1.103959];
     camera1 = camera(3:end);
     ground_truth_ptz(end+1).img_name = image_name;
     ground_truth_ptz(end).para = camera2PTZ(cc, base_rotation, camera1);
