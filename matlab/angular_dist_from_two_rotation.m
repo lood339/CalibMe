a66d5b93c58c69403122809f40bc429305d383c8
zfrruirui <xiaoruiruisen@126.com>
2017-08-11 12:20:31 -0700

    modify file

--- matlab/angular_dist_from_two_rotation.m
+++ matlab/angular_dist_from_two_rotation.m
@@ -1,11 +1,11 @@
 function angular_dist = angular_dist_from_two_rotation(src_R,dst_R)
     scale = 180.0/3.14159;
     
-    r1 = vrrotmat2vec(src_R);
-    r2 = vrrotmat2vec(dst_R);
+    r1 = rotm2eul(src_R);
+    r2 = rotm2eul(dst_R);
     
-    q1 = angle2quat(r1);
-    q2 = angle2quat(r2);
+    q1 = angle2quat(r1(1),r1(2),r1(3));
+    q2 = angle2quat(r2(1),r2(2),r2(3));
     
     val_dot = abs(dot(q1,q2));
     
